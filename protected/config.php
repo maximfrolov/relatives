e7c271607a34bf4fc8685be235373f14eb17a8ad
Maxim Frolov <consumer2@mail.ru>
2016-03-11 14:24:00 +0500

    + конфиг соединения с БД

--- protected/config.php
+++ protected/config.php
@@ -1,3 +1,13 @@
 <?php
 
-return [];
+return [
+    'db' => [
+        'default' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 't4',
+        ],
+    ],
+];
